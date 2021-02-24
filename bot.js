72696ebd263e22ec5513cc6ef5e3a7dbeefdded9
Johannes Nilsson <johannes@nilsson.im>
2021-02-24 09:20:28 +0100

    Separate cmds into functions

--- bot.js
+++ bot.js
@@ -1,22 +1,37 @@
 const { exec } = require('child_process');
 
-const startScript = exec('.././vhserver start');
-const stopScript = exec('.././vhserver stop');
+function start() {
+   const startScript = exec('.././vhserver start');
 
-startScript.stdout.on('data', data => {
-   console.log(data);
-});
+   startScript.stdout.on('data', data => {
+      console.log(data);
+   });
+   
+   startScript.stderr.on('data', data => {
+      console.error(data);
+   });
+}
 
-startScript.stderr.on('data', data => {
-   console.error(data);
-});
+function stop() {
+   const stopScript = exec('.././vhserver stop');
 
-/*
-stopScript.stdout.on('data', data => {
-   console.log(data);
-});
+   stopScript.stdout.on('data', data => {
+      console.log(data);
+   });
 
-stopScript.stderr.on('data', data => {
-   console.error(data);
-});
-*/
+   stopScript.stderr.on('data', data => {
+      console.error(data);
+   });
+}
+
+function restart() {
+   const restartScript = exec('.././vhserver restart');
+
+   restartScript.stdout.on('data', data => {
+      console.log(data);
+   });
+
+   restartScript.stderr.on('data', data => {
+      console.error(data);
+   });
+}
