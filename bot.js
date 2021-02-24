93d63d4f58754ed05ae270ac08eb859df9363c42
Johannes Nilsson <johannes@nilsson.im>
2021-02-24 08:10:54 +0000

    Test basic start/stop cmd

--- bot.js
+++ bot.js
@@ -1,1 +1,22 @@
-console.log('Bot ran..');
+const { exec } = require('child_process');
+
+const startScript = exec('.././vhserver start');
+const stopScript = exec('.././vhserver stop');
+
+startScript.stdout.on('data', data => {
+   console.log(data);
+});
+
+startScript.stderr.on('data', data => {
+   console.error(data);
+});
+
+/*
+stopScript.stdout.on('data', data => {
+   console.log(data);
+});
+
+stopScript.stderr.on('data', data => {
+   console.error(data);
+});
+*/
