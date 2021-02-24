80ecfb78c2d7b015d55ab1aa77329c8e32655367
Johannes Nilsson <johannes@nilsson.im>
2021-02-24 09:37:30 +0100

    Add basic bot functionality

--- bot.js
+++ bot.js
@@ -1,4 +1,41 @@
+require('dotenv').config();
+
 const { exec } = require('child_process');
+const { client } = require('discord.js');
+const PREFIX = '!';
+
+client.on('ready', () => {
+   console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on('message', message => {
+   if (message.author.bot) return; // Don't respond to bot messages
+
+   if(message.content.startsWith(PREFIX)) {
+      const [cmd, ...args] = message.content
+         .trim()
+         .substring(PREFIX.length)
+         .split(/\s+/);
+      
+      if(cmd === 'start') {
+         message.channel.send("Starting the server! Get ready :crossed_swords:");
+         start();
+      }
+
+      if(cmd === 'stop') {
+         message.channel.send("Stopping the server! :skull:");
+         stop();
+      }
+
+      if(cmd === 'restart') {
+         message.channel.send("Restarting the server! Go grab a snack while you wait :meat_on_bone:");
+         restart();
+      }
+   }
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
 
 function start() {
    const startScript = exec('.././vhserver start');
