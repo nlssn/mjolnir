cfe3218c4cfc449924d965a8bba10891df077daa
Johannes Nilsson <johannes@nilsson.im>
2021-02-28 00:47:52 +0100

    Replace .env with config.json

--- bot.js
+++ bot.js
@@ -3,39 +3,37 @@ require('dotenv').config();
 const { exec } = require('child_process');
 const { Client } = require('discord.js');
 const client = new Client();
-const PREFIX = '!';
+const { prefix, token } = require('./config.json');
 
 client.on('ready', () => {
-   console.log(`${client.user.tag} has logged in.`);
+   console.log(`${client.user.tag} is ready.`);
 });
 
 client.on('message', message => {
-   if (message.author.bot) return; // Don't respond to bot messages
+   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-   if(message.content.startsWith(PREFIX)) {
-      const [cmd, ...args] = message.content
-         .trim()
-         .substring(PREFIX.length)
-         .split(/\s+/);
+   const [cmd, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
       
-      if(cmd === 'start') {
-         message.channel.send("Starting the server! Get ready :crossed_swords:");
-         start();
-      }
+   if(cmd === 'start') {
+      message.channel.send("Starting the server! Get ready :crossed_swords:");
+      start();
+   }
 
-      if(cmd === 'stop') {
-         message.channel.send("Stopping the server! :skull:");
-         stop();
-      }
+   if(cmd === 'stop') {
+      message.channel.send("Stopping the server! :skull:");
+      stop();
+   }
 
-      if(cmd === 'restart') {
-         message.channel.send("Restarting the server! Go grab a snack while you wait :meat_on_bone:");
-         restart();
-      }
+   if(cmd === 'restart') {
+      message.channel.send("Restarting the server! Go grab a snack while you wait :meat_on_bone:");
+      restart();
    }
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(token);
 
 
 function start() {
