25677b84d09c2c9c26b9b3f9d9d9f60bd861e704
Johannes Nilsson <johannes@nilsson.im>
2021-02-28 11:17:51 +0100

    Accept the client as an arg

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = {
    name: 'ready',
    once: true,
-   execute() {
+   execute(client) {
       console.log(`${client.user.tag} HAS ARRIVED!`);
       console.log(`(The bot is running, connected to your Discord and ready to be used)`);
    }
