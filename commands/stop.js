22a3027a5e5f709ac21a9820286d117b6dfdb854
Johannes Nilsson <johannes@nilsson.im>
2021-02-28 01:06:22 +0100

    Move stop command to separate file

---
+++ commands/stop.js
@@ -0,0 +1,19 @@
+const { exec } = require('child_process');
+
+module.exports = {
+   name: 'start',
+   description: 'Stops the Valheim server',
+   execute(message, args) {
+      message.channel.send("Stopping the server! :skull:");
+      
+      const script = exec('.././vhserver stop');
+
+      script.stdout.on('data', data => {
+         console.log(data);
+      });
+
+      script.stderr.on('data', data => {
+         console.error(data);
+      });
+   },
+};
