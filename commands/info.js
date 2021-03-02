1514096fb9e1c5816d6ad12f3fd3b7223b1aeca7
Johannes Nilsson <johannes@nilsson.im>
2021-03-02 20:09:22 +0100

    Display the server name

--- commands/info.js
+++ commands/info.js
@@ -1,3 +1,5 @@
+const { server_name } = require('../config.json');
+
 module.exports = {
    name: 'info',
    description: 'Prints info about the Valheim server',
@@ -5,9 +7,10 @@ module.exports = {
       message.channel.send({
          embed: {
             color: 3447003,
-            title: "About the sever",
+            title: `${server_name}`,
             fields: [
                { name: "IP", value: "0.0.0.0" },
+               { name: "Status", value: "Online/Offline"},
                { name: "Players", value: "X / 10" },
                { name: "Uptime", value: "10hours 43minutes"}
             ]
